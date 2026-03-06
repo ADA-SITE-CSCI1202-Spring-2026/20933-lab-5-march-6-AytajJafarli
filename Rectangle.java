b68969a3cd2ee08f4721f628e252e0e1f4d74fcb
aytajjaf <aytajjafarli07@gmail.com>
2026-03-06 13:16:55 +0400

    Question 2

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+
+    int width, height;
+
+    public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    public boolean equals(Object obj) {
+
+        Rectangle rect = (Rectangle) obj;
+
+        if(this.width == rect.width && this.height == rect.height)
+            return true;
+        else
+            return false;
+    }
+}
